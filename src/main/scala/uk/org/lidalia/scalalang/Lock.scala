2ca06854313d92883d52a214e498e81930447c4f
Robert Elliot <rob@lidalia.org.uk>
2015-11-13 13:36:47 +0000

    Made lock work without needing to specify read or write explicitly

--- src/main/scala/uk/org/lidalia/scalalang/Lock.scala
+++ src/main/scala/uk/org/lidalia/scalalang/Lock.scala
@@ -2,12 +2,14 @@ package uk.org.lidalia.scalalang
 
 import java.util.concurrent.locks.ReentrantReadWriteLock
 
-final class Lock {
+final class Lock extends ResourceFactory[Unit] {
 
   private val lock = new ReentrantReadWriteLock()
 
   val readLock = new LockDef(lock.readLock())
   val writeLock = new LockDef(lock.writeLock())
+
+  override def withA[T](work: (Unit) => T) = writeLock.withA(work)
 
   final class LockDef private[Lock] (lock: java.util.concurrent.locks.Lock) extends ResourceFactory[Unit] {
     override def withA[T](work: (Unit) => T): T = {
