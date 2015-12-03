b58e58dd4191f054e8b6286888ee9a7b4b5955ee
Robert Elliot <rob@lidalia.org.uk>
2015-12-03 17:35:29 +0000

    Added more structure to tests

--- src/test/scala/uk/org/lidalia/scalalang/PoolTests.scala
+++ src/test/scala/uk/org/lidalia/scalalang/PoolTests.scala
@@ -22,15 +22,19 @@ class PoolTests extends fixture.FunSuite {
   }
 
   test("result is propagated") { pool =>
+
+    // when
     val result = pool.using { resource =>
       "expected result"
     }
 
+    // then
     assert(result == "expected result")
   }
 
   test("pool throws exception thrown using resource and cleans it up") { pool =>
 
+    // when
     var captured: Resource = null
 
     val exception = new RuntimeException("Oh no")
@@ -43,27 +47,26 @@ class PoolTests extends fixture.FunSuite {
       }
     }
 
+    // then
     assert(intercepted == exception)
     assert(captured.open)
     assert(captured.dirty) // reset was not called
     assert(captured.onErrorCalled.contains(exception))
     assert(pool.size == 1)
-
-    pool.using { resource =>
-      assert(resource == captured)
-    }
   }
 
   test("exception thrown closing resource after failing to open is suppressed exception") { pool =>
 
+    // given
     resourceFactory.failOnOpen = true
     resourceFactory.failOnClose = true
 
+    // when
     val intercepted = intercept[RuntimeException] {
-      pool.using { resource =>
-      }
+      pool.using { resource => }
     }
 
+    // then
     assert(intercepted.getMessage == "Failed to open")
     assert(flattenSuppressed(intercepted).map(_.getMessage).toList == List("Failed to close"))
     assert(pool.size == 0)
@@ -71,23 +74,25 @@ class PoolTests extends fixture.FunSuite {
 
   test("resource is reset after use") { pool =>
 
-    var captured: Resource = null
-
-    pool.using { resource =>
+    // given
+    val dirtyResource = pool.using { resource =>
       resource.dirty = true
-      captured = resource
+      resource
     }
 
+    // expect
     pool.using { resource =>
-      assert(resource == captured)
+      assert(resource == dirtyResource)
       assert(!resource.dirty)
     }
   }
 
   test("pool throws exception thrown resetting resource and closes it") { pool =>
 
+    // given
     resourceFactory.failOnReset = true
 
+    // when
     var captured: Resource = null
     val intercepted = intercept[RuntimeException] {
       pool.using { resource =>
@@ -95,6 +100,7 @@ class PoolTests extends fixture.FunSuite {
       }
     }
 
+    // then
     assert(intercepted.getMessage == "Failed to reset")
     assert(captured.closed)
     assert(pool.size == 0)
@@ -102,14 +108,17 @@ class PoolTests extends fixture.FunSuite {
 
   test("exception thrown closing resource after exception in reset is suppressed exception") { pool =>
 
+    // given
     resourceFactory.failOnClose = true
     resourceFactory.failOnReset = true
 
+    // when
     val intercepted = intercept[RuntimeException] {
       pool.using { resource =>
       }
     }
 
+    // then
     assert(intercepted.getMessage == "Failed to reset")
     assert(flattenSuppressed(intercepted).map(_.getMessage).toList == List("Failed to close"))
     assert(pool.size == 0)
@@ -117,8 +126,10 @@ class PoolTests extends fixture.FunSuite {
 
   test("exception thrown in onError is suppressed exception") { pool =>
 
+    // given
     resourceFactory.failOnReset = true
 
+    // when
     val exception = new RuntimeException("Oh no")
     val intercepted = intercept[RuntimeException] {
       pool.using { resource =>
@@ -126,6 +137,7 @@ class PoolTests extends fixture.FunSuite {
       }
     }
 
+    // then
     assert(intercepted == exception)
     assert(flattenSuppressed(intercepted).map(_.getMessage).toList == List("Failed to handle error"))
     assert(pool.size == 0)
@@ -133,9 +145,11 @@ class PoolTests extends fixture.FunSuite {
 
   test("exception thrown in onClose is suppressed by exception in onError which is suppressed exception") { pool =>
 
+    // given
     resourceFactory.failOnClose = true
     resourceFactory.failOnReset = true
 
+    // when
     val exception = new RuntimeException("Oh no")
     val intercepted = intercept[RuntimeException] {
       pool.using { resource =>
@@ -143,6 +157,7 @@ class PoolTests extends fixture.FunSuite {
       }
     }
 
+    // then
     assert(intercepted == exception)
     assert(flattenSuppressed(intercepted).map(_.getMessage).toList == List("Failed to handle error", "Failed to close"))
     assert(pool.size == 0)
@@ -150,8 +165,10 @@ class PoolTests extends fixture.FunSuite {
 
   test("if check fails after exception close and eject") { pool =>
 
+    // given
     resourceFactory.onCheck = () => Reusable.BROKEN
 
+    // when
     var captured: Resource = null
 
     val exception = new RuntimeException("Oh no")
@@ -164,6 +181,7 @@ class PoolTests extends fixture.FunSuite {
       }
     }
 
+    // then
     assert(intercepted == exception)
     assert(captured.closed)
     assert(captured.dirty) // reset was not called
@@ -173,9 +191,11 @@ class PoolTests extends fixture.FunSuite {
 
   test("if closing after check fails after exception close and eject") { pool =>
 
+    // given
     resourceFactory.onCheck = () => Reusable.BROKEN
     resourceFactory.failOnClose = true
 
+    // when
     val exception = new RuntimeException("Oh no")
 
     val intercepted = intercept[RuntimeException] {
@@ -184,6 +204,7 @@ class PoolTests extends fixture.FunSuite {
       }
     }
 
+    // then
     assert(intercepted == exception)
     assert(flattenSuppressed(intercepted).map(_.getMessage).toList == List("Failed to close"))
     assert(pool.size == 0)
@@ -191,9 +212,11 @@ class PoolTests extends fixture.FunSuite {
 
   test("if check throws exception after exception close and eject") { pool =>
 
+    // given
     val checkException = new RuntimeException("Failed check")
     resourceFactory.onCheck = () => throw checkException
 
+    // when
     val exception = new RuntimeException("Oh no")
 
     val intercepted = intercept[RuntimeException] {
@@ -202,6 +225,7 @@ class PoolTests extends fixture.FunSuite {
       }
     }
 
+    // then
     assert(intercepted == exception)
     assert(flattenSuppressed(intercepted).map(_.getMessage).toList == List("Failed check"))
     assert(pool.size == 0)
@@ -209,10 +233,12 @@ class PoolTests extends fixture.FunSuite {
 
   test("if check throws exception and close throws exception after exception propagate suppressed") { pool =>
 
+    // given
     val checkException = new RuntimeException("Failed check")
     resourceFactory.onCheck = () => throw checkException
     resourceFactory.failOnClose = true
 
+    // when
     val exception = new RuntimeException("Oh no")
 
     val intercepted = intercept[RuntimeException] {
@@ -221,8 +247,8 @@ class PoolTests extends fixture.FunSuite {
       }
     }
 
+    // then
     assert(intercepted == exception)
-
     assert(flattenSuppressed(intercepted).map(_.getMessage).toList == List("Failed check", "Failed to close"))
     assert(pool.size == 0)
   }
