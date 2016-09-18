025f7950683e9f71b79e73958e6d3c2689705f38
Robert Elliot <rob@lidalia.org.uk>
2016-09-18 15:12:38 +0100

    Add missing test to TryFinally suite

--- src/test/scala/uk/org/lidalia/scalalang/TryFinallyTests.scala
+++ src/test/scala/uk/org/lidalia/scalalang/TryFinallyTests.scala
@@ -55,5 +55,19 @@ class TryFinallyTests extends FunSuite {
     assert(thrown.getCause == null)
   }
 
+  test("ignores exception in disposal if same as one in work") {
+    val workException = new Throwable("Work failed")
+    val thrown = intercept[Throwable] {
+      _try {
+        throw workException
+      } _finally {
+        _throw(workException)
+      }
+    }
+    assert(thrown == workException)
+    assert(thrown.getSuppressed.toList.isEmpty)
+    assert(thrown.getCause == null)
+  }
+
   def _throw(t: Throwable): Unit = throw t
 }
